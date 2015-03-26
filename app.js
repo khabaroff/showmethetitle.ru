91c6228db626fd750d76f546cba337ac37ef7880
Sergey Khabaroff <khabaroff@gmail.com>
2015-03-27 00:48:42 +0300

    7th

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-var lines1 = {};
-var lines2 = {};
-var lines3 = {};
-var rnd1;
-var rnd3;
-var rnd2;
-var rnd4;
+(function($) {
+  var documentReady = $.Deferred();
 
-$.when(ajax1(), ajax2(), ajax3()).done(function(a1, a2, a3) {
-  lines1 = a1[0].split("\n");
-  lines2 = a2[0].split("\n");
-  lines3 = a3[0].split("\n");
-
-
-});
-
-
-function ajax1() {
-  return $.ajax({
-    url: "http://habarobots.ru/lab/ttl/01.txt"
+  $(document).ready(function() {
+    documentReady.resolve();
   });
-}
 
-function ajax2() {
-  return $.ajax({
-    url: "http://habarobots.ru/lab/ttl/02.txt"
+  var documentsDeferr = $.when(
+    getDoc('http://habarobots.ru/lab/ttl/01.txt'),
+    getDoc('http://habarobots.ru/lab/ttl/02.txt'),
+    getDoc('http://habarobots.ru/lab/ttl/03.txt'),
+    documentReady
+  );
+
+  documentsDeferr.done(function(a1, a2, a3) {
+    var lines1 = a1[0].split("\n");
+    var lines2 = a2[0].split("\n");
+    var lines3 = a3[0].split("\n");
+
+    var rnd1 = getRnd(lines1);
+    var rnd3 = getRnd(lines3);
+    var rnd2 = getRnd(lines2);
+
+
+    document.querySelector("div").innerHTML = lines1[rnd1] + lines2[rnd2] + lines3[rnd3];
+
+    var rnd4 = Math.ceil(Math.random() * 11) + 1;
+    document.body.className = 'site' + rnd4;
   });
-}
 
-function ajax3() {
-  return $.ajax({
-    url: "http://habarobots.ru/lab/ttl/03.txt"
-  });
-}
+  function getDoc(url) {
+    return $.ajax({
+      url: url
+    });
+  }
+
+  function getRnd(obj) {
+    return Math.floor(obj.length * Math.random());
+  }
 
 
-rnd1 = Math.floor(Math.random() * Object.keys(lines1).length);
-rnd2 = Math.floor(Math.random() * Object.keys(lines2).length);
-rnd3 = Math.floor(Math.random() * Object.keys(lines3).length);
-rnd4 = Math.ceil(Math.random() * 11) + 1 ;
-
-document.body.className='site'+rnd4;
+})(jQuery);
