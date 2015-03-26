c37ffaf9e09552777796002d66fdb0276c6659e3
Sergey Khabaroff <khabaroff@gmail.com>
2015-03-26 23:18:51 +0300

    5th

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
-var lines1 = new Object();
-var lines2 = new Object();
-var lines3 = new Object();
+var lines1 = new Object({});
+var lines2 = new Object({});
+var lines3 = new Object({});
 
-$.ajax({
-  url: "http://habarobots.ru/lab/ttl/01.txt",
-  async: false
-})
-  .done(function( data1 ) {
-      lines1 = data1.split("\n");
-	  //console.log(lines1);
-    }
- );
 
-$.ajax({
-  url: "http://habarobots.ru/lab/ttl/02.txt",
-  async: false
-})
-  .done(function( data2 ) {
-      lines2 = data2.split("\n");
-	  //console.log(lines2);
-    }
- );
+$.when(ajax1(), ajax2(), ajax3()).done(function(a1, a2, a3){
 
- $.ajax({
-  url: "http://habarobots.ru/lab/ttl/03.txt",
-  async: false
-})
-  .done(function( data3 ) {
-      lines3 = data3.split("\n");
-	  //console.log(lines3);
-    }
- );
+  lines1 = a1[0].split("\n");
+  lines2 = a2[0].split("\n");
+  lines3 = a3[0].split("\n");
+});
+
+
+function ajax1() {
+      return $.ajax({
+          url: "http://habarobots.ru/lab/ttl/01.txt"
+      });
+}
+
+function ajax2() {
+      return $.ajax({
+          url: "http://habarobots.ru/lab/ttl/02.txt"
+      });
+}
+
+function ajax3() {
+      return $.ajax({
+          url: "http://habarobots.ru/lab/ttl/03.txt"
+      });
+}
+
 
 rnd1 = Math.floor(Math.random() * Object.keys(lines1).length);
 rnd2 = Math.floor(Math.random() * Object.keys(lines2).length);
